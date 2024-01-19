0fa0f10ecf8873037ad27740a79f8a4411af88c4
Jonathas Marques <149964627+JonathasMarques@users.noreply.github.com>
2024-01-19 12:59:29 -0300

    react axios

--- react-axios/src/routes/Home.jsx
+++ react-axios/src/routes/Home.jsx
@@ -1,4 +1,4 @@
-import axios from "axios"
+import blogFetch from "../axios/config";
 
 import { useState, useEffect } from "react"
 
@@ -8,19 +8,17 @@ import "./Home.css"
 
 const Home = () => {
 
-  const [post, setPost] = useState([])
+  const [posts, setPost] = useState([])
 
   const getPosts = async () => {
    
     try {
       
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
+      const response = await blogFetch.get("/posts");
 
       const data = response.data
       
-      console.log(data)
+      setPost(data);
 
     } catch (error) {
 
@@ -36,7 +34,20 @@ const Home = () => {
   }, [])
 
   return (
-    <div>Home</div>
+    <div className="home">
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? <p>Carregando...</p> : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/post/${post.id}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
   )
 }
 
